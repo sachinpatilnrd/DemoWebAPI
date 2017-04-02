0bba99eec30024fd5b26fadcf13b22c603d0c662
sachinpatilnrd <sachinpatil.nrd@gmail.com>
2017-04-02 13:41:55 +0530

    emp api added

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        EmpDBContext edb = new EmpDBContext();
+        public IEnumerable<Employee> GetEmplyees()
+        {
+            return edb.Employees.ToList();
+        }
+
+    }
+}
