95454f7bf3349887e9c7d0a0fd194a6d39531dfe
sachinpatilnrd <sachinpatil.nrd@gmail.com>
2017-04-02 13:00:06 +0530

    New Project Uploaded

---
+++ WebApplication1/Models/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DBContext
+    {
+        public students GetStudentDetails()
+        {
+            students stud = new students();
+            stud.id = 1;
+            stud.name = "Sachin";
+            stud.standard = "MCA";
+            stud.city = "Pune";
+            return stud;
+        }
+    }
+}
---
+++ WebApplication1/Models/students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class students
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string standard { set; get; }
+        public string city { set; get; }
+
+    }
+}
